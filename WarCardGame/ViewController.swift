2d899eba34c2b45e4a77b9a49799a10ba46fc0ba
natashagp <natasha.paulon@gmail.com>
2020-06-28 18:49:52 -0300

    app War Card Game build with Swift

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Natasha Paulon on 27/06/20.
+//  Copyright © 2020 Natasha Paulon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var leftImageView: UIImageView!
+  
+  @IBOutlet weak var rightImageView: UIImageView!
+  
+  @IBOutlet weak var leftScoreLabel: UILabel!
+  
+  @IBOutlet weak var rightScoreLabel: UILabel!
+  
+  var leftScore = 0
+  var rightScore = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  
+  @IBAction func dealTapped(_ sender: Any) {
+    
+    // Randomize some numbers
+    let leftNumber = Int.random(in: 2...14)
+    let rightNumber = Int.random(in: 2...14)
+    
+    // Update the image views
+    leftImageView.image = UIImage(named: "card\(leftNumber)")
+    rightImageView.image = UIImage(named: "card\(rightNumber)")
+    
+    // Compare the random numbers
+    if leftNumber > rightNumber {
+      // Left side wins
+      leftScore += 1
+      leftScoreLabel.text = String(leftScore)
+    }
+    else if leftNumber < rightNumber {
+      // Right side wins
+      rightScore += 1
+      rightScoreLabel.text = String(rightScore)
+    }
+    else {
+      // Tie
+    }
+    
+  }
+
+}
+
